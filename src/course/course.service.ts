cb8b53741eecb1f3d04717c99c6c62090472c0f4
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-04 13:10:46 +0200

    created the course services

---
+++ src/course/course.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Course } from './course.entity';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { CreateCourseDto } from './create-course.dto';
+
+@Injectable()
+export class CourseService {
+    constructor(
+        @InjectRepository(Course)
+        private courseRepository: Repository<Course>
+    ){}
+
+    createCourse(course : CreateCourseDto): Promise<Course>{
+        return this.courseRepository.save(course);
+    }
+
+    deleteCourse(id: number): Promise<DeleteResult>{
+        return this.courseRepository.delete(id);
+    }
+
+    updateCourse(course: Course): Promise<UpdateResult>{
+        return this.courseRepository.update(course.id, course);
+    }
+}
