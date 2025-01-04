f20adc8371d4aaae26ca7e9f9ea4be8edb837818
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-04 22:09:28 +0200

    mapped the user timetable relationship in the user entity

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Timetable } from "src/timetable/timetable.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User{
@@ -19,4 +20,7 @@ export class User{
 
     @Column()
     password: string;
+
+    @OneToMany(()=>Timetable, (timetable: Timetable)=>timetable.user)
+    timetables: Timetable[]
 }
