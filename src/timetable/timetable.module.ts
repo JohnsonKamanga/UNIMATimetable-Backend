ec1a770c3c9f80c21a2d0dcd372987d6e851cea1
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-04 22:14:55 +0200

    created the timetable module

---
+++ src/timetable/timetable.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TimetableService } from './timetable.service';
+import { TimetableController } from './timetable.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Timetable } from './timetable.entity';
+import { CourseModule } from 'src/course/course.module';
+import { CourseToTimeTable } from 'src/coursetotimetable/coursetotimetable.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Timetable, CourseToTimeTable]), CourseModule],
+  providers: [TimetableService],
+  controllers: [TimetableController]
+})
+export class TimetableModule {}
