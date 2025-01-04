6000454eb8ded37d3574498e22af67fee898cce4
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-04 22:18:19 +0200

    imported all available entities, added database connections and other dependancies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { CourseModule } from './course/course.module';
+import { TimetableModule } from './timetable/timetable.module';
+import { AuthModule } from './auth/auth.module';
+import { CoursetotimetableModule } from './coursetotimetable/coursetotimetable.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env'
+  }),
+TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  useFactory: (configService: ConfigService)=>({
+    type: 'postgres',
+    host: configService.get<string>('DATABASE_HOST'),
+    database: configService.get<string>('DATABASE_NAME'),
+    password: configService.get<string>('DATABASE_PASSWORD'),
+    username: configService.get<string>('DATABASE_USERNAME'),
+    port: configService.get<number>('DATABASE_PORT'),
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+  inject: [ConfigService]
+}),
+UserModule,
+CourseModule,
+TimetableModule,
+AuthModule,
+CoursetotimetableModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
