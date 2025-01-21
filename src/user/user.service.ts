d6a9a884fef5d1bd6997d69ea873f6d2304002d5
JohnsonKamanga <johnsonkamanga2@gmail.com>
2025-01-21 21:34:24 +0200

    added a method to update a user's password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { CreateUserDto } from './create-user.dto';
 import { UpdateNormalUserDetailsDto } from './update-normal-user-details.dto';
-import { genSalt, hash } from 'bcrypt';
+import { compare, genSalt, hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -31,15 +35,42 @@ export class UserService {
   }
 
   //update user's password
-  async updateUserPassword(userid: number, pass
-    : string): Promise<UpdateResult> {
-        //generate password salt
-        const salt = await genSalt(10);
-    
-        //use salt to generate hashed password
-        const password = await hash(pass, salt);
-        
-    return this.userRepository.update(userid, { password });
+  async updateUserPassword(
+    userid: number,
+    passwords: { original_password: string; new_password: string },
+  ): Promise<UpdateResult> {
+    const { original_password, new_password } = passwords;
+    try {
+      //find user
+      const user = await this.userRepository.findOne({ where: { id: userid } });
+
+      if (!user) {
+        throw new NotFoundException(`User with the given id not found`);
+      }
+
+      //compared original password to hashed password
+      const res = await compare(original_password, user.password);
+
+      if (!res) {
+        throw new UnauthorizedException(
+          'Orginal password submitted is incorrect',
+        );
+      }
+
+      //generate password salt
+      const salt = await genSalt(10);
+
+      //use salt to generate hashed password
+      const hashedPassword = await hash(new_password, salt);
+
+      return this.userRepository.update(userid, { password: hashedPassword });
+    } catch (err) {
+      console.error(
+        `An error occured when trying to update the password of the user with id ${userid}: `,
+        err,
+      );
+      return err;
+    }
   }
 
   findUserById(id: number): Promise<User> {
@@ -48,6 +79,9 @@ export class UserService {
 
   async findUserByUsernameWithoutPassword(username: string) {
     const fetchedUser = await this.userRepository.findOneBy({ username });
+    if(!fetchedUser){
+      throw new NotFoundException(`User account with the user name ${username} could not be found`)
+    }
     const { password, ...user } = fetchedUser;
 
     return user;
